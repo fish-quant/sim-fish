cc7e8ec07b66d89eb758744be3267ecb7b3f5e3b
Arthur Imbert <arthur.imbert.pro@gmail.com>
2020-09-30 21:06:38 +0200

    update setup

--- setup.py
+++ setup.py
@@ -10,11 +10,11 @@ from setuptools import setup, find_packages
 
 
 # package description
-DESCRIPTION = "Toolbox for the analysis of smFISH images."
+DESCRIPTION = "Toolbox to simulate smFISH images."
 
 # package version
 VERSION = None
-with open('bigfish/__init__.py', encoding='utf-8') as f:
+with open('simfish/__init__.py', encoding='utf-8') as f:
     for row in f:
         if row.startswith('__version__'):
             VERSION = row.strip().split()[-1][1:-1]
@@ -23,8 +23,6 @@ with open('bigfish/__init__.py', encoding='utf-8') as f:
 # package dependencies
 with open("requirements.txt", encoding='utf-8') as f:
     REQUIREMENTS = [l.strip() for l in f.readlines() if l]
-DEEPLEARNING_REQUIREMENTS = [
-    'tensorflow == 2.2.0']
 
 # long description of the package
 with open("README.md", "r") as f:
@@ -49,17 +47,16 @@ CLASSIFIERS = [
     'License :: OSI Approved :: BSD License']
 
 # setup
-setup(name='big-fish',
+setup(name='sim-fish',
       version=VERSION,
       description=DESCRIPTION,
       long_description=LONG_DESCRIPTION,
       long_description_content_type="text/markdown",
       author='Arthur Imbert',
       author_email='arthur.imbert.pro@gmail.com',
-      url='https://github.com/fish-quant/big-fish',
+      url='https://github.com/fish-quant/sim-fish',
       packages=find_packages(),
       license='BSD 3-Clause License',
       python_requires='>=3.6',
       install_requires=REQUIREMENTS,
-      extras_require={'deeplearning': DEEPLEARNING_REQUIREMENTS},
       classifiers=CLASSIFIERS)
