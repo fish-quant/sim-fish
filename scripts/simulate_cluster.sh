c3e9c431090a3022879d6318df45e410c4326c70
Arthur Imbert <arthur.imbert.pro@gmail.com>
2021-07-02 10:56:38 +0200

    bash script to simulate clusters

--- scripts/simulate_cluster.sh
+++ scripts/simulate_cluster.sh
@@ -8,10 +8,10 @@ output_directory='/Users/arthur/output/2021_simulations'
 # python script
 script='/Users/arthur/sim-fish/scripts/python/simulate_range_noise.py'
 
-# ### Cluster 0.05 - 0.4 ###
+# ### Cluster 0.05 - 0.4 (5 spots in cluster) ###
 
 # parameters
-experiment="cluster_all"
+experiment="cluster_all_5"
 n_images=100
 image_z=32
 image_y=128
@@ -25,7 +25,7 @@ n_spots=100
 random_n_spots=0
 n_clusters=1
 random_n_clusters=0
-n_spots_cluster=10
+n_spots_cluster=5
 random_n_spots_cluster=0
 centered_cluster=1
 sigma_z=100
@@ -47,6 +47,39 @@ python "$script" "$output_directory" "$experiment" "$n_images" \
         "$amplitude" \
         "$noise_level" "$random_min" "$random_max"
 
+# ### Cluster 0.05 - 0.4 (10 spots in cluster) ###
+
+# parameters
+experiment="cluster_all_10"
+n_spots_cluster=10
+
+python "$script" "$output_directory" "$experiment" "$n_images" \
+        "$image_z" "$image_y" "$image_x" \
+        "$subpixel_factors_z" "$subpixel_factors_y" "$subpixel_factors_x" \
+        "$voxel_size_z" "$voxel_size_yx" \
+        "$n_spots" "$random_n_spots" \
+        "$n_clusters" "$random_n_clusters" \
+        "$n_spots_cluster" "$random_n_spots_cluster" "$centered_cluster" \
+        "$sigma_z" "$sigma_yx" "$random_sigma" \
+        "$amplitude" \
+        "$noise_level" "$random_min" "$random_max"
+
+# ### Cluster 0.05 - 0.4 (15 spots in cluster) ###
+
+# parameters
+experiment="cluster_all_15"
+n_spots_cluster=15
+
+python "$script" "$output_directory" "$experiment" "$n_images" \
+        "$image_z" "$image_y" "$image_x" \
+        "$subpixel_factors_z" "$subpixel_factors_y" "$subpixel_factors_x" \
+        "$voxel_size_z" "$voxel_size_yx" \
+        "$n_spots" "$random_n_spots" \
+        "$n_clusters" "$random_n_clusters" \
+        "$n_spots_cluster" "$random_n_spots_cluster" "$centered_cluster" \
+        "$sigma_z" "$sigma_yx" "$random_sigma" \
+        "$amplitude" \
+        "$noise_level" "$random_min" "$random_max"
 
 # python script
 script='/Users/arthur/sim-fish/scripts/python/simulate_range_spots_cluster.py'
