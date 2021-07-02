043feacb6d9a8a00216eec694ca339fcb382d75d
Arthur Imbert <arthur.imbert.pro@gmail.com>
2021-07-02 11:00:09 +0200

    change version

--- simfish/__init__.py
+++ simfish/__init__.py
@@ -12,4 +12,4 @@ The simfish package.
 # MINOR: new features
 # PATCH: backwards compatible bug fixes
 # MAJOR.MINOR.PATCHdev means a version under development
-__version__ = "0.1.0dev"
+__version__ = "0.1.0"
