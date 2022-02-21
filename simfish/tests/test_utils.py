dfcab8a78b05da0c478a2ef34a83cbf9a429d4c8
Arthur Imbert <arthur.imbert.pro@gmail.com>
2022-02-21 15:54:58 +0100

    initialize unit test utils

---
+++ simfish/tests/test_utils.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Author: Arthur Imbert <arthur.imbert.pro@gmail.com>
+# License: BSD 3 clause
+
+"""
+Unitary tests for simfish.utils module.
+"""
+
+
+# TODO add test for load_extract_template function
+# TODO add test for read_index_template function
+# TODO add test for template building
